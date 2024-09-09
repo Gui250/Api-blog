686c53ed09ef3e7d8315046b3cd36b048bc5f63a
Guilherme Moreno <guilhermemoreno200@gmail.com>
2024-09-09 12:02:39 -0300

    implementações feitas

--- src/controladores/autorControlador.ts
+++ src/controladores/autorControlador.ts
@@ -6,6 +6,72 @@ export default class AutorControlador {
   //  private autores: Autor[] = autores 
     
     listar(req: Request, res: Response) {
-        return res.json(autores);
+      return res.json(autores);
+    }
+
+    
+    detalhar(req: Request, res: Response) { 
+        const { id } = req.params;
+
+        const autor = autores.find((elemento) => {
+          return elemento.id === id
+        })
+
+        if(!autor) { 
+            return res.status(404).json({
+              mensagem: 'Autor não encontrado!'
+          })
+        }
+
+        return res.json(autor);
+    }
+
+
+    cadastrar(req: Request, res: Response) {
+      const { nome, idade } = req.body; 
+
+      if(!nome || !idade) { 
+        res.status(400).json({mensagem: 'O nome e a idade do autor são obrigatórios!'});
+      }
+
+      const autor = new Autor({
+        nome, idade
+      })
+
+      autores.push(autor);
+
+
+      return res.status(201).json(autor);
+    }
+
+
+    editar(req: Request, res: Response) { 
+        // buscar o autor que vai ser editado
+        const { id } = req.params;
+        const { nome, idade } = req.body; 
+
+        if(!nome || !idade) { 
+          return res.status(400).json({
+            mensagem: 'O nome e a idade do autor são obrigatórios!'
+          })
+        }
+
+
+
+        const autor = autores.find((elemento) => {
+          return elemento.id === id
+        })
+
+        if(!autor) { 
+            return res.status(404).json({
+              mensagem: 'Autor não encontrado!'
+          })
+        }
+        //alterar o nome e a idade do autor
+        autor.nome = nome;
+
+        autor.idade = idade; 
+
+        return res.status(204).json()
     }
 }
