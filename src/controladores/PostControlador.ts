f086727539eb9b5e44102e2ac1dc5f13b1de686b
Guilherme Moreno <guilhermemoreno200@gmail.com>
2024-09-10 10:00:08 -0300

    implementação das rotas dos posts

---
+++ src/controladores/PostControlador.ts
@@ -0,0 +1,52 @@
+import {  Request, Response  } from "express"; 
+import {  autores, posts  } from "../bancoDeDados"; 
+import Autor from '../modelos/Autor'; 
+import Post from "../modelos/Post";
+
+
+export default class PostControlador { 
+    listar(req: Request, res: Response) { 
+        return res.json(posts);
+    }
+
+    detalhar(req: Request, res: Response) { 
+        const { id } = req.params; 
+
+
+        const post = posts.find((elemento) => { 
+            return elemento.id === id; 
+        })
+
+        if(!post) { 
+            return res.status(404).json({mensagem: 'Post não existe'});
+        }
+
+        return res.json(post); 
+    } 
+
+    cadastrar(req: Request, res: Response) {
+        const { titulo, descricao, autor_id } = req.body;
+
+        if(!titulo || !descricao || !autor_id) { 
+            return res.status(400).json({mensagem: "Todos os campos da postagem são obrigatórios"});
+        }
+
+        const autor = autores.find((elemento) => { 
+            return elemento.id === autor_id;
+        })
+
+
+        if(!autor) { 
+            return res.status(404).json({mensagem: 'O autor não existe'});
+        }
+
+        const post = new Post({
+            titulo,
+            descricao,
+            autor
+        })
+
+
+        return res.status(201).json(post);
+    }
+}
--- src/rotas.ts
+++ src/rotas.ts
@@ -1,9 +1,12 @@
 import {  Router  } from 'express'
 import AutorControlador from './controladores/AutorControlador';
+import PostControlador from './controladores/PostControlador';
 
 const rotas = Router(); 
 
 const autorControlador = new AutorControlador();
+const postControlador = new PostControlador();
+
 
 rotas.get('/autores', autorControlador.listar);
 rotas.get('/autores/:id', autorControlador.detalhar);
@@ -11,4 +14,8 @@ rotas.post('/autores', autorControlador.cadastrar);
 rotas.put('/autores/:id', autorControlador.editar);
 rotas.delete('/autores/:id', autorControlador.excluir);
 
+
+rotas.get('/postagens'); 
+rotas.get('/postagens/:id', postControlador.detalhar);
+rotas.post('/postagens', postControlador.cadastrar); 
 export default rotas; 
